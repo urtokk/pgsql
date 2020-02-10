e8d0e0fe83e08dd456438b19cba5cfe68b8ea060
Torben <xendios@gmail.com>
2020-02-10 15:11:23 +0100

    feat: added some structs to handle configuration

--- src/common/configs.rs
+++ src/common/configs.rs
@@ -1,2 +1,39 @@
 extern crate config;
+extern crate serde;
 
+use std::net::IpAddr;
+use std::collections::HashMap;
+use config::{
+    Config,
+    ConfigError,
+    File
+};
+
+/// Struct to manage entries with connection information to a database
+#[derive(Debug, Deserialize)]
+pub struct Entry {
+    flavor: String,
+    address: IpAddr,
+    port: u16,
+    user: String,
+    password: String,
+    database: String
+}
+
+/// postgrsql settings
+// TODO: integrate client options
+#[derive(Debug, Deserialize)]
+pub struct postgresql {
+    binary: String
+}
+
+/// Struct to manage configuration file with global configurations and database entries
+#[derive(Debug, Deserialize)]
+pub struct settings{
+    postgresql: postgresql,
+    entries: HashMap<String, Entry>,    
+}
+
+impl settings{
+
+}
--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,6 @@
+#[macro_use]
+extern crate serde_derive;
+
 mod common;
 mod database;
 
